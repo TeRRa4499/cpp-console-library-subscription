c21a5deaf3304b0bd0a5077048588be9e6fe6a26
TeRRa4499 <danyfoxy87@gmail.com>
2025-05-28 21:36:13 +0300

    Добавлен вывод общей информации о задании

--- main-project/main.cpp
+++ main-project/main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
-    std::cout << "Hello\n";
+    cout << "Laboratory work #4. GIT\n";
+    cout << "Variant #5. Internet Protocol\n";
+    cout << "Author: Daniil Kazakov\n";
     return 0;
 }

0524e8a9bfb543964ea6e7c7f8e68a62d1adaa45
Daniil Kazakov <danyfoxy87@gmail.com>
2025-05-29 10:55:03 +0300

    Добавлены тестовые данные

--- main-project/main.cpp
+++ main-project/main.cpp
@@ -7,5 +7,6 @@ int main()
     cout << "Laboratory work #4. GIT\n";
     cout << "Variant #5. Internet Protocol\n";
     cout << "Author: Daniil Kazakov\n";
+    cout << "Group: 24PI1D\n";
     return 0;
 }

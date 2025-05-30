5da63e9253e2d8455696309dc3cce4af21cda3f6
Daniil Kazakov <danyfoxy87@gmail.com>
2025-05-30 22:37:58 +0300

    process заглушки

---
+++ main-project/process.cpp
@@ -0,0 +1,10 @@
+#include "processing.h"
+int process(internet_protocol* array[], int size)
+{
+	int i,answ=0;
+	//for (i = 0; i < size; i++)
+	//{
+		//array[i]->
+	//	}
+	return answ;
+}
---
+++ main-project/processing.h
@@ -0,0 +1,5 @@
+#ifndef PROCESSING_H
+#define PROCESSING_H
+#include "internet_protocol.h"
+int process(internet_protocol* array[], int size);
+#endif
